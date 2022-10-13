1a15acbc1ba40ec872316c31f7cc5ed191785dcc
Matt Stanbrell <mattstanbrell@gmail.com>
2022-10-13 16:13:47 +0100

    Working primes

--- primes.py
+++ primes.py
@@ -1,6 +1,17 @@
-"""List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
     list = []
+    num = 2
+    while len(list) < number_of_primes:
+        if is_prime(num):
+            list.append(num)
+        num += 1
+
     return list
+
+
+def is_prime(num):
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
